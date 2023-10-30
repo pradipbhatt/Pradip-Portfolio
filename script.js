a91cf5a2155ff5b6c35cf6895406e6917708273d
PRADIP BHATT <pradip10bhatt@gmail.com>
2023-10-30 12:36:50 +0545

    About Journey Section Added

--- script.js
+++ script.js
@@ -21,3 +21,67 @@ function rotateCuboid() {
 
 setInterval(rotateCuboid, 50); // Adjust the interval for rotation speed
 
+
+
+
+// Get references to buttons and content divs
+const educationBtn = document.getElementById("educationBtn");
+const experienceBtn = document.getElementById("experienceBtn");
+const volunteeringBtn = document.getElementById("volunteeringBtn");
+const awardBtn = document.getElementById("awardBtn");
+
+const educationContent = document.getElementById("educationContent");
+const experienceContent = document.getElementById("experienceContent");
+const volunteeringContent = document.getElementById("volunteeringContent");
+const awardContent = document.getElementById("awardContent");
+
+// Add click event listeners to buttons
+educationBtn.addEventListener("click", () => {
+    hideAllContent();
+    educationContent.style.display = "block";
+});
+
+experienceBtn.addEventListener("click", () => {
+    hideAllContent();
+    experienceContent.style.display = "block";
+});
+
+volunteeringBtn.addEventListener("click", () => {
+    hideAllContent();
+    volunteeringContent.style.display = "block";
+});
+
+awardBtn.addEventListener("click", () => {
+    hideAllContent();
+    awardContent.style.display = "block";
+});
+
+// Function to hide all content divs
+function hideAllContent() {
+    educationContent.style.display = "none";
+    experienceContent.style.display = "none";
+    volunteeringContent.style.display = "none";
+    awardContent.style.display = "none";
+}
+
+// Initially, show the Education content
+educationContent.style.display = "block";
+
+
+
+
+function toggleSection(button) {
+  const section = button.parentElement.parentElement;
+  const deleteButton = section.querySelector('.delete-button');
+  const addButton = section.querySelector('.add-button');
+
+  if (button.classList.contains('delete-button')) {
+      section.style.display = 'none';
+      deleteButton.style.display = 'none';
+      addButton.style.display = 'block';
+  } else {
+      section.style.display = 'block';
+      deleteButton.style.display = 'block';
+      addButton.style.display = 'none';
+  }
+}
